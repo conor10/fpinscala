23095b6ea70c5b7a0511c41c726bcefa3ada43af
Conor <conor10@gmail.com>
2015-01-03 19:01:14 +0800

    Completed Chapter 4 Option exercises.

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,15 +4,27 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(x) => Some(f(x))
+    case _ => None
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case Some(x) => x
+    case _ => default
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = map(f)getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElseMatch[B>:A](ob: => Option[B]): Option[B] = this match {
+    case Some(x) => this
+    case _ => ob
+  }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] = map(Some(_)).getOrElse(ob)
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(x => if (f(x)) Some(x) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
@@ -38,11 +50,64 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap(m => mean(xs.map(x => math.pow(x - m, 2))))
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def map2Match[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (Some(x), Some(y)) => Some(f(x, y))
+      case (_, _) => None
+    }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap(x => b.map(y => f(x, y)))
+
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    def loop(a: List[Option[A]], acc: List[A]) : Option[List[A]] = {
+      a match {
+//        case Some(x) :: Nil => Some(acc :+ x)
+        case Some(x) :: xs => loop(xs, acc :+ x)
+        case Nil => Some(acc)
+        case _ => None
+      }
+    }
+    loop(a, List())
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    def loop(a: List[A], acc: List[B]): Option[List[B]] = {
+      a match {
+//        case x :: Nil => f(x) match {
+//          case Some(x) => Some(acc :+ x)
+//          case _ => None
+//        }
+        case x :: xs => f(x) match {
+          case Some(x) => loop(xs, acc :+ x)
+          case _ => None
+        }
+        case Nil => Some(acc)
+        case _ => None
+      }
+    }
+
+    loop(a, List())
+  }
+
+  def traverseMap[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    a match {
+      case Nil => Some(Nil)
+      case x :: xs => map2(f(x), traverseMap(xs)(f))(_::_)
+    }
+  }
+
+  // Sequence in terms of traverse
+  def seqTraverse[A](a: List[Option[A]]): Option[List[A]] = {
+    traverse(a)((x) => x)
+  }
+
+  def lift[A,B](f: A => B): Option[A] => Option[B] = _ map f
 }
+
---
+++ exercises/src/test/scala/errorhandling/OptionTest.scala
@@ -0,0 +1,94 @@
+package errorhandling
+
+import org.scalatest.FunSuite
+
+import fpinscala.errorhandling._
+import fpinscala.errorhandling.Option._
+
+/**
+ * Option unit tests.
+ */
+class OptionTest extends FunSuite {
+  test("map") {
+    val f = (x: Int) => x * 2
+    assert(Some(1).map(f) === Some(2))
+    assert(None.map(f) === None)
+  }
+
+  test("getOrElse") {
+    assert(Some(1).getOrElse(2) === 1)
+    assert(None.getOrElse(2) === 2)
+  }
+
+  test("flatMap") {
+    val f = (x: Int) => Some(x * 2)
+    assert(Some(1).flatMap(f) === Some(2))
+    assert(None.flatMap(f) === None)
+  }
+
+  test("orElse") {
+    assert(Some(1).orElse(Some(2)) === Some(1))
+    assert(None.orElse(Some(2)) === Some(2))
+  }
+
+  test("orElseMatch") {
+    assert(Some(1).orElseMatch(Some(2)) === Some(1))
+    assert(None.orElseMatch(Some(2)) === Some(2))
+  }
+
+  test("filter") {
+    val f = (x: Int) => x % 2 == 0
+    assert(Some(1).filter(f) === None)
+    assert(Some(2).filter(f) === Some(2))
+  }
+
+  test("variance") {
+    assert(Some(2.0) === variance(List(1.0, 2.0, 3.0, 4.0, 5.0)))
+  }
+
+  test("map2") {
+    val f = (x: Int, y: Int) => x + y
+    assert(Some(3) === map2(Some(1), Some(2))(f))
+    assert(None === map2(Some(1), None)(f))
+    assert(None === map2(None, Some(2))(f))
+    assert(None === map2(None, None)(f))
+  }
+
+  test("map2Match") {
+    val f = (x: Int, y: Int) => x + y
+    assert(Some(3) === map2Match(Some(1), Some(2))(f))
+    assert(None === map2Match(Some(1), None)(f))
+    assert(None === map2Match(None, Some(2))(f))
+    assert(None === map2Match(None, None)(f))
+  }
+
+  test("sequence") {
+    assert(Some(List(1, 2, 3)) === sequence(List(Some(1), Some(2), Some(3))))
+    assert(None === sequence(List(None)))
+    assert(Some(Nil) === sequence(Nil))
+    assert(None === sequence(List(Some(1), Some(2), None)))
+    assert(None === sequence(List(Some(1), None, Some(3))))
+  }
+
+  test("traverse") {
+    val f = (x: Int) => if (x > 0) Some(x.toString) else None
+    assert(Some(List("1", "2", "3")) === traverse(List(1, 2, 3))(f))
+    assert(None === traverse(List(1, 0, 3))(f))
+    assert(Some(Nil) === traverse(List())(f))
+  }
+
+  test("traverseMap") {
+    val f = (x: Int) => if (x > 0) Some(x.toString) else None
+    assert(Some(List("1", "2", "3")) === traverseMap(List(1, 2, 3))(f))
+    assert(None === traverseMap(List(1, 0, 3))(f))
+    assert(Some(Nil) === traverseMap(List())(f))
+  }
+
+  test("seqTraverse") {
+    assert(Some(List(1, 2, 3)) === seqTraverse(List(Some(1), Some(2), Some(3))))
+    assert(None === seqTraverse(List(None)))
+    assert(Some(Nil) === seqTraverse(Nil))
+    assert(None === seqTraverse(List(Some(1), Some(2), None)))
+    assert(None === seqTraverse(List(Some(1), None, Some(3))))
+  }
+}
